4d241799986b8a61f0fb3b91b04444d17ce4a44d
HowXu <dev@howxu.cn>
2026-08-02 23:01:57 +0800

    fix: register 5 item models in MMCRItemModelProvider

--- src/main/java/cn/howxu/mmcr/datagen/MMCRItemModelProvider.java
+++ src/main/java/cn/howxu/mmcr/datagen/MMCRItemModelProvider.java
@@ -5,6 +5,8 @@ import cn.howxu.mmcr.registry.MMCRRegistries;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelLocationUtils;
 import net.minecraft.core.Holder;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Item;
@@ -19,6 +21,21 @@ public final class MMCRItemModelProvider extends ModelProvider {
 
     @Override
     protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        itemModels.itemModelOutput.accept(
+                MMCRRegistries.CONTROLLER_BLOCK_ITEM.get(),
+                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(MMCRRegistries.CONTROLLER_BLOCK.get())));
+        itemModels.itemModelOutput.accept(
+                MMCRRegistries.CASING_BLOCK_ITEM.get(),
+                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(MMCRRegistries.CASING_BLOCK.get())));
+        itemModels.itemModelOutput.accept(
+                MMCRRegistries.ITEM_BUS_BLOCK_ITEM.get(),
+                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(MMCRRegistries.ITEM_BUS_BLOCK.get())));
+        itemModels.itemModelOutput.accept(
+                MMCRRegistries.FLUID_HATCH_BLOCK_ITEM.get(),
+                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(MMCRRegistries.FLUID_HATCH_BLOCK.get())));
+        itemModels.itemModelOutput.accept(
+                MMCRRegistries.ENERGY_HATCH_BLOCK_ITEM.get(),
+                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(MMCRRegistries.ENERGY_HATCH_BLOCK.get())));
     }
 
     @Override
