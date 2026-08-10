7c3353e9f2ef6caf5d9db306977080b677f519a5
HowXu <dev@howxu.cn>
2026-08-10 08:14:36 +0800

    feat: add structure claim registry

---
+++ src/main/java/cn/howxu/mmcr/internal/multiblock/ComponentClaimPolicy.java
@@ -0,0 +1,11 @@
+package cn.howxu.mmcr.internal.multiblock;
+
+/**
+ * Ownership behavior for a stateful multiblock component.
+ *
+ * @author howxu <dev@howxu.cn>
+ */
+public enum ComponentClaimPolicy {
+    EXCLUSIVE,
+    SHARED_SERIALIZED
+}
