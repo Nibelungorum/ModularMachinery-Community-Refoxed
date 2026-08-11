8b9553b5b6897099a7db8033c422e435daac5d7f
HowXu <dev@howxu.cn>
2026-08-11 20:09:52 +0800

    docs: clean

--- run/scripts/startup_scripts/mmcr_demo.js
+++
@@ -1,9 +0,0 @@
-const demoCube = new MMCR_MACHINE_BUILDER('mmcr:demo_cube')
-    .localizedName('Demo Cube')
-    .pattern(`
-        CCC
-        CFC
-        CCC
-    `, { C: 'mmcr:casing', F: 'mmcr:controller' });
-
-demoCube.register();
