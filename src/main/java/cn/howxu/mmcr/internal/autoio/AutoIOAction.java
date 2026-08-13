572916877aff61c6930d31924c486c20b2bd5bc5
HowXu <dev@howxu.cn>
2026-08-13 12:57:14 +0800

    feat: add auto io config model

---
+++ src/main/java/cn/howxu/mmcr/internal/autoio/AutoIOAction.java
@@ -0,0 +1,11 @@
+package cn.howxu.mmcr.internal.autoio;
+
+/**
+ * Server-side mutation requested by the Auto IO control page.
+ *
+ * @author howxu <dev@howxu.cn>
+ */
+public enum AutoIOAction {
+    TOGGLE_ENABLED,
+    TOGGLE_SIDE
+}
---
+++ src/main/java/cn/howxu/mmcr/internal/autoio/AutoIOCapabilityType.java
@@ -0,0 +1,13 @@
+package cn.howxu.mmcr.internal.autoio;
+
+/**
+ * Auto IO capability categories supported by machine ports.
+ *
+ * @author howxu <dev@howxu.cn>
+ */
+public enum AutoIOCapabilityType {
+    ITEM,
+    FLUID,
+    ENERGY,
+    GAS
+}
